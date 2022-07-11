331584dd0416aebf21e1124a43e3b71c8f8a2a16
User <ulla102121@gmail.com>
2022-07-11 12:27:08 +0800

    321

---
+++ camp-project-master/src/components/About.vue
@@ -0,0 +1,54 @@
+<script setup>
+import {ArrowRightIcon} from "@heroicons/vue/outline";
+
+const skills = [
+	{
+		title: '珍珠奶茶',
+		description: '冰涼濃厚的奶茶搭配上富含嚼勁的珍珠。',
+	},
+	{
+		title: '芒果雪花冰',
+		description: '綿密的牛奶冰屑淋上滿滿的芒果醬與新鮮的芒果塊',
+	},
+	{
+		title: '薯條',
+		description: '果然被油炸得金黃的薯條最能擄獲我的舌頭,再怎麼油都無所謂',
+	},
+	{
+		title: '壽司',
+		description: '炙燒原味都好吃',
+	},
+	{
+		title: '泡麵',
+		description: '便宜又好吃,cp直很高',
+	},
+]
+</script>
+
+<template>
+
+	<section id="about" class="pt-32 md:px-16 bg-gray-800">
+		<div class="flex flex-col md:flex-row justify-between items-center md:px-8 md:mb-4">
+			<h1 class="text-2xl md:text-4xl text-white font-bold">
+				關於食物
+			</h1>
+			<button
+				class="my-8 mr-3 border-b border-red-200 pb-2 flex items-center text-xl text-white">
+				所有經歷
+				<ArrowRightIcon class="ml-1 w-5 h-5"/>
+			</button>
+		</div>
+		<div class="flex flex-wrap justify-start">
+			<div
+				class="w-full md:w-1/3"
+				v-for="skill in skills">
+				>
+				<div
+					class="px-10 sm:px-20 py-3 sm:py-6 shadow-lg w-11/12 mx-auto bg-white rounded z-20 relative">
+					<h4 class="text-orange-400 text-lg text-center mb-2">{{ skill.title }}</h4>
+					<p class="text-orange-400 text-lg text-center mb-2">{{ skill.description }}</p>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
---
+++ camp-project-master/src/components/Header.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ArrowRightIcon, ChevronDoubleDownIcon} from '@heroicons/vue/solid'
+</script>
+<template>
+	<section class="bg-orange-200 flex flex-col justify-center items-center md:px-20 min-h-screen">
+		<div class="flex flex-col w-full py-12 px-4 lg:px-0 justify-center items-end">
+			<p class="text-3xl text-orange-800 font-extrabold">哈囉！我是<span class="text-orange-800">施淳瑜</span></p>
+			<h1 class="text-5xl text-right text-red-600 mt-1 font-extrabold">
+				一個喜歡吃美食的高中生
+			</h1>
+			<p class="text-3xl text-red-700 font-medium pt-8">
+				[食物介紹]
+			</p>
+			<a
+				href="#contact"
+				class="mt-3 flex items-center text-xl font-regular pb-3">
+				聯絡我
+				<ArrowRightIcon class="ml-1 w-8 h-5"/>
+			</a>
+		</div>
+
+		<a href="#activities" class="animate-bounce cursor-pointer">
+			<chevron-double-down-icon class="w-18 h-16"/>
+		</a>
+	</section>
+</template>
